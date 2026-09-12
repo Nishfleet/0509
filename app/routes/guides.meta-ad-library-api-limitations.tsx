acd98bda1808f1e63da8ff0a39b4c60d220afd8e
fleet-salvage <fleet-salvage@localhost>
2026-09-12 14:05:33 +0530

    wip(salvage): pi-issue-0509-2967 success/0

--- app/routes/guides.meta-ad-library-api-limitations.tsx
+++ app/routes/guides.meta-ad-library-api-limitations.tsx
@@ -37,6 +37,8 @@ import {
 } from "~/lib/seo";
 import { SUPPORT_EMAIL, SUPPORT_MAILTO } from "~/lib/support";
 
+import "~/styles/marketing.css";
+
 const PATHNAME = "/guides/meta-ad-library-api-limitations";
 
 /**
